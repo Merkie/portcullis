52a773b6ec4c3c3f9bc32d302517ee8050b6ddfa
Merkie <merkie@post.com>
2024-01-27 17:47:20 -0600

    Add title to dashboard page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -7,6 +7,10 @@
 	}
 </script>
 
+<svelte:head>
+	<title>Dashboard | Portcullis</title>
+</svelte:head>
+
 <p class="m-16">This is a protected Route!</p>
 
 <button class="btn-auth m-16 w-96" on:click={signOut}>Sign Out</button>
