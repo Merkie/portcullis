4ca8807f1ec00c42e5d5e0a1ff5716dfaa9a7ef6
Merkie <merkie@post.com>
2024-01-27 14:09:00 -0600

    Add login and register routes

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Mail } from 'lucide-svelte';
+</script>
+
+<svelte:head>
+	<title>Login | Portcullis</title>
+</svelte:head>
+
+<h1 class="mb-8 font-display text-4xl font-semibold">Login</h1>
+
+<button class="auth-btn mb-4">
+	<Mail size={20} />
+	<span>Continue with Email</span>
+</button>
+
+<button class="auth-btn mb-8">
+	<img src="/images/google-icon-small.png" width={20} alt="Google Logo" />
+	<span>Continue with Google</span>
+</button>
+
+<p>
+	Don't have an account? <a href="/register" class="link">Register</a>
+</p>
