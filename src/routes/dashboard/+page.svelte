f06a21afb5019a534916ef6ccc6579083e064d0d
Merkie <merkie@post.com>
2024-01-27 15:16:18 -0600

    Add sign out functionality to dashboard page

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	async function signOut() {
+		await fetch('/api/auth/signout', {
+			method: 'POST'
+		}).then((res) => res.json());
+		window.location.reload();
+	}
+</script>
+
+<p class="m-16">This is a protected Route!</p>
+
+<button class="btn-auth m-16 w-96" on:click={signOut}>Sign Out</button>
