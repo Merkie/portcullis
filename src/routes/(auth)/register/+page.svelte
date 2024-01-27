0d25ac85c1308b6eeb63333988de07c9ca5f33ac
Merkie <merkie@post.com>
2024-01-27 14:31:57 -0600

    Update auth buttons styling

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -4,12 +4,12 @@
 
 <h1 class="mb-8 font-display text-4xl font-semibold">Register</h1>
 
-<a href="/register/with-email" class="auth-btn mb-4">
+<a href="/register/with-email" class="btn-auth mb-4">
 	<i class="bi bi-envelope"></i>
 	<span>Continue with Email</span>
 </a>
 
-<button class="auth-btn mb-8" disabled={true}>
+<button class="btn-auth mb-8" disabled={true}>
 	<img src="/images/google-icon-small.png" width={20} alt="Google Logo" />
 	<span>Continue with Google</span>
 </button>
