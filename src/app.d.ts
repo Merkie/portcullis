232cdbeb75730d50187e1bd09a5e41df61ef204d
Merkie <merkie@post.com>
2024-01-27 15:15:51 -0600

    Add PrismaClient and User interfaces to Locals in app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
-// See https://kit.svelte.dev/docs/types#app
+import type { User, PrismaClient } from '@prisma/client';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			prisma: PrismaClient;
+			user: User;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
