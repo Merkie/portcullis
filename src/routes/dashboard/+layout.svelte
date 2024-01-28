12918dae59e904953219d5d137194fe3881b8a76
Merkie <merkie@post.com>
2024-01-28 09:49:08 -0600

    add dashboard

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import {
+		CreditCard,
+		Files,
+		FormInput,
+		Home,
+		MessageSquare,
+		PhoneCall,
+		Plug,
+		Users
+	} from 'lucide-svelte';
+</script>
+
+<svelte:head>
+	<title>Dashboard | Portcullis</title>
+</svelte:head>
+
+<main class="flex h-screen w-screen">
+	<!-- Side Nav -->
+	<div class="flex h-full w-[300px] flex-col border-r border-neutral-900">
+		<!-- Logo -->
+		<a href="/" class="flex items-center gap-4 p-8 pb-12 font-display text-2xl font-semibold">
+			<img width={25} src="/images/portcullis-logo.png" alt="Portcullis Logo" />
+			<span class="translate-y-[1px]">Portcullis</span>
+		</a>
+		<!-- Nav Items -->
+		<a href={'/dashboard'} class="flex items-center gap-3 p-3 px-8 text-xl">
+			<Home />
+			<span>Home</span>
+		</a>
+		<a href={'/dashboard/clients'} class="flex items-center gap-3 p-3 px-8 text-xl">
+			<Users />
+			<span>Clients</span>
+		</a>
+		<div class="my-6 h-[1px] border-t border-neutral-900"></div>
+		<p class="mb-2 ml-8 font-display font-medium text-neutral-500">Paths</p>
+		<a href={'/dashboard/chat'} class="flex items-center gap-3 p-3 px-8 text-xl">
+			<MessageSquare />
+			<span>Chat</span>
+		</a>
+		<a href={'/dashboard/files'} class="flex items-center gap-3 p-3 px-8 text-xl">
+			<Files />
+			<span>Files</span>
+		</a>
+		<a href={'/dashboard/forms'} class="flex items-center gap-3 p-3 px-8 text-xl">
+			<FormInput />
+			<span>Forms</span>
+		</a>
+		<a href={'/dashboard/billing'} class="flex items-center gap-3 p-3 px-8 text-xl">
+			<CreditCard />
+			<span>Billing</span>
+		</a>
+		<a href={'/dashboard/helpdesk'} class="flex items-center gap-3 p-3 px-8 text-xl">
+			<PhoneCall />
+			<span>Helpdesk</span>
+		</a>
+		<div class="my-6 h-[1px] border-t border-neutral-900"></div>
+		<p class="mb-2 ml-8 font-display font-medium text-neutral-500">Management</p>
+		<a href={'/dashboard/integrations'} class="flex items-center gap-3 p-3 px-8 text-xl">
+			<Plug />
+			<span>Integrations</span>
+		</a>
+	</div>
+	<div class="flex flex-1 flex-col">
+		<slot><!-- optional fallback --></slot>
+	</div>
+</main>
